086317ee063b44b8e17cdf94c6c9b31687152156
Marvin Correia <marvin.correia@devgo.org>
2021-06-29 16:37:28 -0100

    update app style

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,5 +1,64 @@
 import React from 'react';
+import { Container, ProductTable, Total } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>PRODUCT</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-coreracer-masculino/05/NQQ-4635-205/NQQ-4635-205_zoom2.jpg?ts=1612440481&ims=326x"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito legal</strong>
+              <span>R$190,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,66 @@
 import React from 'react';
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-coreracer-masculino/05/NQQ-4635-205/NQQ-4635-205_zoom2.jpg?ts=1612440481&ims=326x"
+          alt="Tenis legal"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$190,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-coreracer-masculino/05/NQQ-4635-205/NQQ-4635-205_zoom2.jpg?ts=1612440481&ims=326x"
+          alt="Tenis legal"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$190,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-coreracer-masculino/05/NQQ-4635-205/NQQ-4635-205_zoom2.jpg?ts=1612440481&ims=326x"
+          alt="Tenis legal"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$190,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-coreracer-masculino/05/NQQ-4635-205/NQQ-4635-205_zoom2.jpg?ts=1612440481&ims=326x"
+          alt="Tenis legal"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$190,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
